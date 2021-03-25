6d4410e7f6e61ccb8073b04e019e9295f04487fc
Pikdays <byt.quan@gmail.com>
2021-03-25 21:41:23 +0800

    Initial Commit

---
+++ Shared/USAFootballStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  USAFootballStoreApp.swift
+//  Shared
+//
+//  Created by Pikdays on 2021/3/25.
+//
+
+import SwiftUI
+
+@main
+struct USAFootballStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
